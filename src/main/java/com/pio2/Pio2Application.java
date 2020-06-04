251822922ddd1cc04536db12848407e44f2cb678
miguel <miguel.negron@gmail.com>
2020-06-04 15:04:15 +0200

    primer commit desde consola

---
+++ src/main/java/com/pio2/Pio2Application.java
@@ -0,0 +1,13 @@
+package com.pio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pio2Application.class, args);
+	}
+
+}
