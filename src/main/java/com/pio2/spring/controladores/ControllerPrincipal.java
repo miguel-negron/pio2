9a3c19634b82d9518608cb476725ff63fa6fb43c
miguel <miguel.negron@gmail.com>
2020-06-08 13:59:29 +0200

    Form monitor solo-dni funcionando

--- src/main/java/com/pio2/spring/controladores/ControllerPrincipal.java
+++ src/main/java/com/pio2/spring/controladores/ControllerPrincipal.java
@@ -3,14 +3,14 @@ package com.pio2.spring.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import com.pio2.spring.repositorios.CursoRepository;
+import com.pio2.spring.entidades.Monitor;
 import com.pio2.spring.repositorios.MonitorRepository;
-import com.pio2.spring.repositorios.NinyoRepository;
 import com.pio2.spring.servicios.NinyoServiceDB;
-
-import antlr.collections.List;
 
 @Controller
 public class ControllerPrincipal {
@@ -33,6 +33,26 @@ public class ControllerPrincipal {
 		model.addAttribute("listaMonitores", repositorioMonitores.findAll());
 		return "indexMonitores";
 	}
+	
+	@GetMapping("/monitor/new")
+	public String controllerAnyadirMonitor(Model model) {
+		model.addAttribute("monitorForm", new Monitor());
+		return "formMonitor";
+	}
+	
+	@PostMapping("/monitor/new/submit")
+	public String nuevoEmpleadoSubmit(/*@Valid*/ @ModelAttribute("empleadoForm") Monitor nuevoMonitor,
+										BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			return "formMonitor";
+		} else {
+			repositorioMonitores.save(nuevoMonitor);
+			return "redirect:/monitores";
+		}
+
+	}
+
 	
 	@GetMapping("/login")
 	public String log(Model model) {
