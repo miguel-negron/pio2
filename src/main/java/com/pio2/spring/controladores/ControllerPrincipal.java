9ad33400abce4b8abd0b89f9c96d5dba107c9a29
miguel <miguel.negron@gmail.com>
2020-06-13 20:49:47 +0200

    Index mejorado, pero no lo suficiente

--- src/main/java/com/pio2/spring/controladores/ControllerPrincipal.java
+++ src/main/java/com/pio2/spring/controladores/ControllerPrincipal.java
@@ -10,33 +10,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.pio2.spring.entidades.Monitor;
 import com.pio2.spring.repositorios.MonitorRepository;
+import com.pio2.spring.servicios.CursoServiceDB;
 import com.pio2.spring.servicios.MonitorServiceDB;
 import com.pio2.spring.servicios.NinyoServiceDB;
 
 @Controller
 public class ControllerPrincipal {
-	
+
 	@Autowired
 	NinyoServiceDB servicioNinyo;
-	
+
 	@Autowired
 	MonitorServiceDB servicioMonitor;
-	
+
+	@Autowired
+	CursoServiceDB servicioCurso;
+
 	@GetMapping("/")
 	public String controller1(Model model) {
 		model.addAttribute("listaNinyos", servicioNinyo.findAll());
 		model.addAttribute("listaMonitores", servicioMonitor.findAll());
+		model.addAttribute("listaCursos", servicioCurso.findAll());
+
 		return "index";
 	}
-	
+
 	@GetMapping("/login")
 	public String log(Model model) {
 		model.addAttribute("listaNinyos", servicioNinyo.findAll());
 		return "login";
 	}
-	
-	//@GetMapping("/ninyo/new")
+
+	/* 
+	@GetMapping("/ninyo/new")
 	public String crearNinyo(Model model) {
 		return "form";
 	}
+	*/
 }
